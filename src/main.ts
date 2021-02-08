37c5bb743d4a6f5d4ffa3649a33e8c5f2cae8454
Fredrik Nicol <fredrik.nicol@gmail.com>
2021-02-08 12:47:24 +0100

    Expose a function to run from Node

---
+++ src/main.ts
@@ -0,0 +1,60 @@
+import { exec } from 'child_process';
+import * as path from 'path';
+
+type Solution = {
+  solutionFile: string;
+};
+
+type Project = {
+  projectFile: string;
+};
+
+type Options = {
+  configFile: string;
+  verify?: boolean;
+};
+
+function tsTypeGen(options: Solution & Options): Promise<void>;
+function tsTypeGen(options: Project & Options): Promise<void>;
+function tsTypeGen(options: (Solution | Project) & Options) {
+  const args: string[] = [
+    JSON.stringify(path.join(__dirname, '..', 'bin', 'TSTypeGen.dll')),
+  ];
+
+  if ('configFile' in options) {
+    args.push(`--cfg=${JSON.stringify(options.configFile)}`);
+  } else {
+    throw new Error('You must specify a TSTypeGen config file');
+  }
+
+  if (options.verify) {
+    args.push('--verify');
+  }
+
+  return new Promise<void>((resolve, reject) => {
+    exec(`dotnet ${args.join(' ')}`, (error, stdout, stderr) => {
+      if (error) {
+        if (stderr) {
+          process.stdout.write(stderr);
+          reject(new Error());
+        } else {
+          reject(error);
+        }
+      } else {
+        if (stdout) {
+          process.stdout.write(stdout);
+        } else {
+          console.log(
+            options.verify
+              ? 'All type definitions verified'
+              : 'No type definitions changed'
+          );
+        }
+
+        resolve();
+      }
+    });
+  });
+}
+
+export default tsTypeGen;
