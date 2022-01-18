98a35395c37b25941c0e00c80a70eff883a9a2cc
Anders Ekdahl <anders@commercemind.se>
2022-01-18 10:30:04 +0100

    Update comment regarding Babel and const enums

--- src/TSTypeGen/Config.cs
+++ src/TSTypeGen/Config.cs
@@ -15,12 +15,14 @@ namespace TSTypeGen
         public Dictionary<string, string> TypeMappings { get; } = new Dictionary<string, string>();
         public Dictionary<string, string> PropertyWrappers { get; set;  } = new Dictionary<string, string>();
         public string CustomTypeScriptIgnoreAttributeFullName { get; set; }
-        // Babel doesn't support const enums but there's one wierd trick where if you wrap the const enum in a template
-        // string then Babel stops complaining. So if you do:
-        // const x: TheEnum;
-        // Babel won't be happy. But if you do:
-        // const x: `${TheEnum}`;
-        // Babel won't complain because it doesn't understand the type.
+        // Babel doesn't support ambient const enums but there's one wierd trick where if you wrap the const enum in a template
+        // string then const enums becomes literal string unions instead. E.g. you'll have to do this when const enums are not supported:
+        // const x: TheEnum = 'value';
+        // Instead of:
+        // const x: TheEnum = TheEnum.Value;
+        // This makes Typescipt a bit sad, unless the enum is converted into a template literal type:
+        // const x: `${TheEnum}` = 'value';
+        // Then both Babel and Typscript will be satisfied
         public bool WrapConstEnumsInTemplateStrings { get; set; }
 
         public static Config ReadFromFile(string path)
