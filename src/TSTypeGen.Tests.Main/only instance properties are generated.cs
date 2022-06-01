f2901a1ae5138c7e2eee4fe439658d1a2ee0add0
jobook <johan.book@avensia.se>
2022-06-01 11:17:56 +0200

    Suppress build warnings for unused event

--- src/TSTypeGen.Tests.Main/only instance properties are generated.cs
+++ src/TSTypeGen.Tests.Main/only instance properties are generated.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using TSTypeGen.Tests.Shared;
+﻿using TSTypeGen.Tests.Shared;
 
 namespace TSTypeGen.Tests.Main
 {
@@ -11,12 +8,16 @@ namespace TSTypeGen.Tests.Main
         public int Prop1 { get; set; }
         public void Method1() { }
         public int Field1;
+#pragma warning disable CS0067
         public event System.EventHandler Event1;
+#pragma warning restore CS0067
 
         public static int Prop2 { get; set; }
         public static void Method2() { }
         public static int Field2;
+#pragma warning disable CS0067
         public static event System.EventHandler Event2;
+#pragma warning restore CS0067
 
         public int this[int x] { get { return x; } set { } }
     }
